3b210b2efee0dfa7eeb51a3e87935e6b4a8bc791
Computadora <computadora@MacBook-Pro-de-Computadora.local>
2021-02-20 20:24:14 -0300

    second commit

--- src/components/Credentials.js
+++ src/components/Credentials.js
@@ -1,3 +1,4 @@
 export default {
   APIKEY: "AIzaSyCkCom8ScNvtDqlRQPK7JcyxAZFZf4KEXs",
+  EMAILJS_USER: 'user_xMrAjIwOBtBRGRHAmCpkt',
 };
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,4 +1,55 @@
 import React from "react";
+import emailjs from 'emailjs-com'
+import Credentials from './Credentials'
+
+
+export default function EmailForm() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_l6so5vg', e.target, Credentials.EMAILJS_USER)
+      .then((result) => {
+          console.log(result.text);
+          alert(result.text + ', mensaje enviado!')
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  }
+
+  return (
+    <>
+      <div className='form-wrapper'>
+        <div className='container'>
+          <form onSubmit={sendEmail}>
+            <div className='singleItem'>
+              <label htmlFor="name">Nombre</label>
+              <input type='text' className='name' placeholder='Tu nombre...' name='name'/>
+            </div>
+            <div className='singleItem'>
+            <label htmlFor="email">Email</label>
+            <input type='text' className='email' placeholder='Tu correo electrónico...' name='email' required/>
+            </div>
+            <div className='singleItem'>
+            <label htmlFor="subject">Asunto</label>
+            <input type='text' className='asunto' placeholder='El motivo de tu consulta...' name='subject'/>
+            </div>
+            <div className="textArea singleItem">
+            <label htmlFor="message">Mensaje</label>
+            <textarea type='text' rows='5' cols='30' placeholder='Tu mensaje...' name='message'/>
+            </div>
+            <div className="btn">
+                <button type="submit">Enviar</button>
+            </div>
+          </form>
+        </div>  
+      </div>
+    </>
+  );
+}
+
+/*
 import axios from "axios";
 
 class EmailForm extends React.Component {
@@ -135,3 +186,5 @@ class EmailForm extends React.Component {
   }
 }
 export default EmailForm;
+*/
+
